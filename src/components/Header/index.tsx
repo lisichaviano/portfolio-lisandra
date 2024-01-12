94c67e446529650b6fbc7435981d22a223a0cf90
lisichaviano <lisichaviano98@gmail.com>
2024-01-13 00:49:25 +0100

    Separated Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,27 @@ import Link from "../Link";
 
 export const Header = () => {
   return (
-    <div className="w-full h-[92px] flex items-center justify-between">
-      <div className="h-[92px] flex items-center pl-12 gap-3 w-1/4">
-        <div className="bg-firebrick w-4 h-4 overflow-hidden mix-blend-normal " />
-        <p className="text-6xl font-syne">Lisandra's Portfolio</p>
-      </div>
+    <header className="w-full h-[92px] z-20">
+      <div className="flex items-center justify-between">
+        <div className="h-[92px] flex items-center pl-12 w-1/4">
+          <Link
+            additionalClassNAme="text-2xl font-syne hover:text-black active:text-black"
+            href={"/"}
+          >
+            <div className="flex items-center gap-3">
+              <div className="bg-firebrick w-4 h-4 overflow-hidden" />
+              Lisandra's Portfolio
+            </div>
+          </Link>
+        </div>
 
-      <div className="flex items-center gap-3 pr-12">
-        <Link href={"/"}>ABOUT ME</Link>
-        <Link href={"/"}>RESUME</Link>
-        <Link href={"/"}>PORTFOLIO</Link>
-        <Link href={"/"}>CONTACT</Link>
+        <div className="flex items-end gap-3 pr-12">
+          <Link href={"/"}>ABOUT ME</Link>
+          <Link href={"/resume"}>RESUME</Link>
+          <Link href={"/"}>PORTFOLIO</Link>
+          <Link href={"/"}>CONTACT</Link>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
