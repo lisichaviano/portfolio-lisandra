3baff633e1eeda76bb88f8f042c5017fb050e223
lisichaviano <lisichaviano98@gmail.com>
2024-01-15 21:47:02 +0100

    Created new icons chevron

---
+++ src/components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,18 @@
+function ChevronLeftIcon({ scale = 1 }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24 * scale}
+      height={24 * scale}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M10.454 12l4.6 4.6L14 17.654 8.346 12 14 6.346 15.054 7.4l-4.6 4.6z"
+      ></path>
+    </svg>
+  );
+}
+
+export default ChevronLeftIcon;
---
+++ src/components/icons/ChevronRightIcon.tsx
@@ -0,0 +1,18 @@
+function ChevronRightIcon({ scale = 1 }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24 * scale}
+      height={24 * scale}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M12.946 12l-4.6-4.6L9.4 6.346 15.054 12 9.4 17.654 8.346 16.6l4.6-4.6z"
+      ></path>
+    </svg>
+  );
+}
+
+export default ChevronRightIcon;
