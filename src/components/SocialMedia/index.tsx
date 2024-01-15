d5c9793a41908d7d16930889e6acb48f9751fd92
lisichaviano <lisichaviano98@gmail.com>
2024-01-15 18:53:47 +0100

    Remove / from images

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -2,10 +2,10 @@ const SocialMedia = () => {
   return (
     <div className="flex gap-2">
       <a href="https://www.linkedin.com/in/lisandra-chaviano/">
-        <img className="w-6 h-6" src="/image@2x.png" alt="" />
+        <img className="w-6 h-6" src="image@2x.png" alt="" />
       </a>
       <a className="mt-1" href="https://github.com/lisichaviano">
-        <img className="w-5 h-5" src="/githubIcon.png" alt="" />
+        <img className="w-5 h-5" src="githubIcon.png" alt="" />
       </a>
     </div>
   );
